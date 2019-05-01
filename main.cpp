2be6e0b44ef912c67bc6c0a718612747c485ab98
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-05-01 18:13:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,37 @@ int main(){
 	char choice;
 	int len=0;
 	
-	cout << endl;
-	cout << "Add to list (a)." << endl;
-	cout << "Done list item (d)." << endl;
-	cout << "Print list (p)." << endl;
-	cout << "Exit list app (x)." << endl;
-	cout << "What do you want to do: ";
-	cin >> choice;
-	cout << endl;
-	
-	switch(choice){
-		case 'a':
+	while (choice !='x'){
+		cout << endl;
+		cout << "Add to list (a)." << endl;
+		cout << "Done list item (d)." << endl;
+		cout << "Print list (p)." << endl;
+		cout << "Exit list app (x)." << endl;
+		cout << "What do you want to do: ";
+		cin >> choice;
+		cout << endl;
+		
+		switch(choice){
+			case 'a':
+				
+				break;
 			
-	
-	
+			case 'd':
+			
+				break;
+				
+			case 'p':
+				
+				break;
+				
+			case 'x':
+				
+				break;
+			
+			default:
+				cout << "invalid selection..." << endl;
+				break;
+		}
+	}
 	return 0;
 }
